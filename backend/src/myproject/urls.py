36c6ed6aaec7aeab6e000c6880bbac36eaf61306
Defendi <tiagodefendidasilva@gmail.com>
2024-08-14 13:54:55 -0300

    configurando

--- backend/src/myproject/urls.py
+++ backend/src/myproject/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('signup/', views.signup, name='signup' ),
-    path('login/', views.login, name='login' ),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
