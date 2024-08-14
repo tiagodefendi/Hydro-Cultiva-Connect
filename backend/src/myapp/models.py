338bd86cb61a62a4c5a83d7e1358a454f0991770
Defendi <tiagodefendidasilva@gmail.com>
2024-08-14 13:54:42 -0300

    reset

--- backend/src/myapp/models.py
+++ backend/src/myapp/models.py
@@ -1,9 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-
-class User(AbstractBaseUser):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)  # O campo email deve ser único
-    password = models.CharField(max_length=100)
-
-    USERNAME_FIELD = 'email'  # Define o campo de login como email
--- backend/src/myapp/views.py
+++ backend/src/myapp/views.py
@@ -1,9 +1,4 @@
 from django.shortcuts import render, redirect
-from .models import User
-from django.contrib import messages
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login as auth_login
 
 # TODO: register new user
 def signup(request):
@@ -14,4 +9,6 @@ def login(request):
     return render(request, 'login.html')
 
 def home(request):
+    
+
     return render(request, 'index.html')
