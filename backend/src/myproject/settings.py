932cab84def7b73ce6bf8f46aedd4cb35e1f8def
Defendi <tiagodefendidasilva@gmail.com>
2024-08-14 13:54:15 -0300

    database path

--- backend/src/myproject/settings.py
+++ backend/src/myproject/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database/db.sqlite3',
     }
 }
 
