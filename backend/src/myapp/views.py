9f08a1bd5aafc78ee5d758a329d1d6ac16a0c141
Defendi <tiagodefendidasilva@gmail.com>
2024-08-21 22:13:03 -0300

    📨
    - insert new user into database

--- backend/src/myapp/views.py
+++ backend/src/myapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+
 
 def add_device(request):
     if request.method == 'POST':
@@ -16,15 +19,34 @@ def add_property(request):
 def properties(request):
     return render(request, 'properties.html')
 
-# TODO: register new user
+#TODO: verify SQL injections
 def signup(request):
     if request.method == 'POST':
-        name: str = request.POST.get('name')
+        username: str = request.POST.get('name')
         email: str = request.POST.get('email')
         password: str = request.POST.get('password')
         confirm_password: str = request.POST.get('confirm_password')
 
-        print(name, '-', email, ':', password, confirm_password, '=?', confirm_password==password)
+        print(username, '-', email, ':', password, confirm_password, '=?', confirm_password==password)
+
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                    return render(request, 'signup.html', {'error': 'Nome de usuário já existe'})
+
+            if User.objects.filter(email=email).exists():
+                    return render(request, 'signup.html', {'error': 'E-mail já está em uso'})
+
+            try:
+                user:User = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                return redirect('login')
+            except:
+                return render(request, 'signup.html', {'error': 'Usuário já existe'})
+        else:
+            return render(request, 'signup.html', {'error': 'As senhas não coincidem'})
+
+
     return render(request, 'signup.html')
 
 # TODO: make login logical
