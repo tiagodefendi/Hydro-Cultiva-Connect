e1415560c5849e5ce9dd1b048445b7eb0b8bd99e
Defendi <tiagodefendidasilva@gmail.com>
2024-08-23 14:12:49 -0300

    new models

--- backend/src/myapp/models.py
+++ backend/src/myapp/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+
+class Property(models.Model):
+    name: models.CharField = models.CharField(max_length=100)
+    description: models.TextField = models.TextField()
+    latitude: models.FloatField = models.FloatField()
+    longitude: models.FloatField = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
+
+class Devices(models.Model):
+    type: models.CharField = models.CharField(max_length=100)
+    name: models.CharField = models.CharField(max_length=100)
+    key: models.CharField = models.CharField(max_length=11)
