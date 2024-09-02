0043543b8a6ced80b66a3bfbb320ab9aaff5c89b
JoaoRicardoMarques <joaoricardomarques@alunos.utfpr.edu.br>
2024-09-02 20:42:51 -0300

    base de dados populada

--- database/populated_database.py
+++ database/populated_database.py
@@ -2,20 +2,32 @@ import duckdb
 import sys
 
 if __name__ == "__main__":
-    usuario=[
-        (1,"Joao","joao@gmail.com","A%sS3wnd"),
-        (2,"Pedro","pedro@gmail.com","ASHSswd"),
-        (3,"Ricardo","ricardo@gmail.com","#UEWH2e"),
+    duckPath = '~/Hydro-Cultiva-Connect/database/database'
+    usuarios=[
+      #  (1,"Joao","joao@gmail.com","A3sS3wnd"),
+       # (2,"Pedro","pedro@gmail.com","ASHSswd"),
+        #(3,"Ricardo","ricardo@gmail.com","#UEWH2e"),
     ]
-    propriedade=[
-        (1,1,"Fazenda Santa Barbara",-1111111,-999999),
-        (2,2,"Instancia Laço de Ouro",-373434,436383),
-        (3,3,"Fazenda Espelho D'Água",343435,-434355),
+    propriedades=[
+     #   (1,1,"Fazenda Santa Barbara",-1111111,-999999),
+      #  (2,2,"Instancia Laço de Ouro",-373434,436383),
+        (3,3,"Fazenda Espelho Dagua",343435,-434355),
     ]
     dispositivos=[
-        (1,1,"regador","51984515195151"),
-        (2,2,"regador","51891981811518"),
+     #   (1,1,"regador","51984515195151"),
+      #  (2,2,"regador","51891981811518"),
         (3,3,"regador","19819519511117"),
     ]
-    duckPath="~/"
-    con=duckdb.connect(database=)
+    
+    con=duckdb.connect(database=duckPath, read_only=False)
+
+    for usuario in usuarios:
+        con.sql(f"INSERT INTO USUARIO (ID,EMAIL,NOME,SENHA) VALUES ({usuario[0]},'{usuario[1]}','{usuario[2]}','{usuario[3]}')")
+
+    for propriedade in propriedades:
+        con.sql(f"INSERT INTO PROPRIEDADE (ID,ID_USUARIO,NOME,LATITUDE,LONGITUDE) VALUES ({propriedade[0]},{propriedade[1]},'{propriedade[2]}','{propriedade[3]}','{propriedade[4]}')")
+
+    for dispositivo in dispositivos:
+        con.sql(f"INSERT INTO DISPOSITIVO (ID,ID_PROPRIEDADE,NOME,CODIGO) VALUES ({dispositivo[0]},{dispositivo[1]},'{dispositivo[2]}','{dispositivo[3]}')")
+
+    con.close()
